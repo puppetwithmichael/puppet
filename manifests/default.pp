9a91a7ed67c5c031acd3570b188a303a3fca2770
Michael <cougarmc1@yahoo.com>
2017-01-05 20:19:21 -0600

    added puppet to the 2 machines

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,13 +1,20 @@
 node 'puppetmaster.local' {
+  include ::puppet
+  include ::puppet::master
   include ::redis
   include ::users::michael
 }
 
 node 'puppetagent.local' {
+  include ::puppet
   include ::users::michael
+  host { 'puppet':
+    ip => '192.168.1.77',
+  }
 }
 
 node default {
+  include ::puppet
   include ::users::michael 
 }
 
